7020a1dd12f062d7c4a7a23a16ba56060bac1e72
Carl Karsten <carl@personnelware.com>
2017-04-05 18:23:19 -0500

    sha512sum --check the .iso file put on the usb stick.

--- scripts/mk_usb_installer.sh
+++ scripts/mk_usb_installer.sh
@@ -8,10 +8,18 @@ dev=sdc
 wget -N http://ftp.nl.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/boot.img.gz
 wget -N http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/$iso
 
+wget -N http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/SHA512SUMS
+
+
 # zcat boot.img.gz|sudo dcfldd of=/dev/$dev conv=fdatasync
 
 pmount /dev/$dev
-cp $iso syslinux.cfg preseed.cfg  /media/$dev
+cp $iso syslinux.cfg preseed.cfg /media/$dev
+
+cd /media/$dev
+sha512sum --check <(grep $iso $OLDPWD/SHA512SUMS)
+cd -
+
 pumount $dev
 
 
