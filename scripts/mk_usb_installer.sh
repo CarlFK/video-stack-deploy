f839eb5c9ca93f6cf4d3c612add396b5c25dd500
Carl Karsten <carl@personnelware.com>
2017-04-05 16:16:23 -0500

    about the same as what the docs say.

---
+++ scripts/mk_usb_installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+# build a usb installer: debian, preseed, ansible
+
+iso=debian-8.7.1-amd64-netinst.iso
+dev=sdc
+
+wget -N http://ftp.nl.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/boot.img.gz
+wget -N http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/$iso
+
+# zcat boot.img.gz|sudo dcfldd of=/dev/$dev conv=fdatasync
+
+pmount /dev/$dev
+cp $iso syslinux.cfg preseed.cfg  /media/$dev
+pumount $dev
+
+
+
