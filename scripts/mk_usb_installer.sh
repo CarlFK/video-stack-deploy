f3131030c6f105b8fdc4ee862927cf660183bff7
Carl Karsten <carl@personnelware.com>
2017-04-11 02:22:03 -0500

    stretch not jessie.

--- scripts/mk_usb_installer.sh
+++ scripts/mk_usb_installer.sh
@@ -2,14 +2,12 @@
 
 # build a usb installer: debian, preseed, ansible
 
-iso=debian-8.7.1-amd64-netinst.iso
+iso=debian-stretch-DI-rc3-amd64-netinst.iso
 dev=sdc
 
-wget -N http://ftp.nl.debian.org/debian/dists/stable/main/installer-amd64/current/images/hd-media/boot.img.gz
-wget -N http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/$iso
-
-wget -N http://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/SHA512SUMS
-
+wget -N http://ftp.debian.org/debian/dists/stretch/main/installer-amd64/current/images/hd-media/boot.img.gz
+wget -N http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd/$iso
+wget -N http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd/SHA512SUMS
 
 zcat boot.img.gz|sudo dcfldd of=/dev/$dev
 # conv=fdatasync
