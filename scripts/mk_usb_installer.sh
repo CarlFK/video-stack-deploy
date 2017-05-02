5465af499393603494dcbae2a4bc8bae2b53ab6a
Carl Karsten <carl@personnelware.com>
2017-05-02 16:12:26 -0500

    Add zesty support.

--- scripts/mk_usb_installer.sh
+++ scripts/mk_usb_installer.sh
@@ -2,12 +2,25 @@
 
 # build a usb installer: debian, preseed, ansible
 
-iso=debian-stretch-DI-rc3-amd64-netinst.iso
+# iso=debian-stretch-DI-rc3-amd64-netinst.iso
+iso=ubuntu-17.04-server-amd64.iso
+
 dev=sdc
 
-wget -N http://ftp.debian.org/debian/dists/stretch/main/installer-amd64/current/images/hd-media/boot.img.gz
-wget -N http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd/$iso
-wget -N http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd/SHA512SUMS
+
+# wget -N http://ftp.debian.org/debian/dists/stretch/main/installer-amd64/current/images/hd-media/boot.img.gz
+# wget -N http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd/$iso
+# wget -N http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd/SHA512SUMS
+
+wget -N http://archive.ubuntu.com/ubuntu/dists/zesty/main/installer-amd64/current/images/hd-media/boot.img.gz
+wget -N http://releases.ubuntu.com/zesty/$iso
+wget -N http://releases.ubuntu.com/zesty/SHA256SUMS
+
+sha256sum --check <(grep $iso SHA256SUMS)
+
+echo pumount /dev/$dev
+sudo dcfldd if=$iso of=/dev/$dev
+exit
 
 zcat boot.img.gz|sudo dcfldd of=/dev/$dev
 # conv=fdatasync
@@ -19,7 +32,8 @@ cp $iso syslinux.cfg d-i/jessie/preseed.cfg /media/$dev
 sed -i "/^DEFAULT.*/s/^.*$/DEFAULT $1/" /media/$dev/syslinux.cfg
 
 cd /media/$dev
-sha512sum --check <(grep $iso $OLDPWD/SHA512SUMS)
+# sha512sum --check <(grep $iso $OLDPWD/SHA512SUMS)
+sha256sum --check <(grep $iso $OLDPWD/SHA256SUMS)
 cd -
 
 pumount $dev
