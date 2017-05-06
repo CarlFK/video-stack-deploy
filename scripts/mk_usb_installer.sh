2229484cd61cb182e6a76b2fbf96942a51f2e285
Carl Karsten <carl@personnelware.com>
2017-05-05 22:43:18 -0500

    wrap bash vars in {} to avoid problems

--- scripts/mk_usb_installer.sh
+++ scripts/mk_usb_installer.sh
@@ -26,55 +26,56 @@ preseed="url=$(hostname):8000"
 # where to get what:
 
 suite=stretch
-bootimg_loc=http://ftp.debian.org/debian/dists/$suite/main/installer-amd64/current/images
+bootimg_loc=http://ftp.debian.org/debian/dists/${suite}/main/installer-amd64/current/images
 iso=debian-stretch-DI-rc3-amd64-netinst.iso
 iso_loc=http://cdimage.debian.org/cdimage/stretch_di_rc3/amd64/iso-cd
 
 # suite=xenial
-# bootimg_loc=http://archive.ubuntu.com/ubuntu/dists/$suite/main/installer-amd64/current/images/
+# bootimg_loc=http://archive.ubuntu.com/ubuntu/dists/${suite}/main/installer-amd64/current/images/
 # iso=ubuntu-16.04.2-server-amd64.iso
-# iso_loc=http://releases.ubuntu.com/$suite
+# iso_loc=http://releases.ubuntu.com/${suite}
 
 # the rest should just work.
 
 # get and veriy the boot image
 # (hd-media dir because that is bunred into the SHA256SUMS file)
-wget -N --directory-prefi hd-media $bootimg_loc/hd-media/boot.img.gz
-curl -OJ $bootimg_loc/SHA256SUMS
+wget -N --directory-prefi hd-media ${bootimg_loc}/hd-media/boot.img.gz
+curl -OJ ${bootimg_loc}/SHA256SUMS
 # pull the line out for the 1 file and verify it
 grep hd-media/boot.img.gz SHA256SUMS > boot.img.gz.SHA256SUM
 sha256sum --check boot.img.gz.SHA256SUM
 
 # cd so most of the .deb's are local
-wget -N $iso_loc/$iso
-curl -OJ $iso_loc/SHA256SUMS
-grep $iso SHA256SUMS > $iso.SHA256SUM
-sha256sum --check $iso.SHA256SUM
+wget -N ${iso_loc}/${iso}
+curl -OJ ${iso_loc}/SHA256SUMS
+grep ${iso} SHA256SUMS > ${iso}.SHA256SUM
+sha256sum --check ${iso}.SHA256SUM
 
-zcat hd-media/boot.img.gz|sudo dcfldd of=/dev/$dev
+zcat hd-media/boot.img.gz|sudo dcfldd of=/dev/${dev}
 # or good ol dd
-# zcat boot.img.gz|sudo dd of=/dev/$dev conv=fdatasync
+# zcat boot.img.gz|sudo dd of=/dev/${dev} conv=fdatasync
 
-pmount /dev/$dev
+pmount /dev/${dev}
 
 # append appends to append, preseed location too.
-sed '/^APPEND/s/$/ ${preseed} ${appends}/' syslinux.cfg > /media/$dev/syslinux.cfg
+# tee so I can see what gets written out.
+sed "/^APPEND/s/$/ ${preseed} ${appends}/" syslinux.cfg | tee /media/${dev}/syslinux.cfg
 
 # copy the preseed files in case of problems serving them over the net.
 # 'just' fis the APPAND line and the early/late stuff and off you go.
-cp -a d-i/$suite/* /media/$dev
+cp -a d-i/${suite}/* /media/${dev}
 
 # this doesn't work for Ubuntu.
 # The .iso is too big and sometimes boot is an iso9660 fs so RO?!!
 # boot.img only has 782M of free space.
 # so put it on a 2nd usb stick, or the unused space on the 8gig usb stick.
-cp $iso $iso.SHA256SUM /media/$dev
+cp ${iso} ${iso}.SHA256SUM /media/${dev}
 
-cd /media/$dev
+cd /media/${dev}
 
 # check the iso image again, make sure it copied ok.
-sha256sum --check $iso.SHA256SUM
+sha256sum --check ${iso}.SHA256SUM
 
 cd -
-pumount /dev/$dev
+pumount /dev/${dev}
 
