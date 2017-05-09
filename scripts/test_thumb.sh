268e5d177db6e2d730d42d6e9f5600fc4ecb4d11
Carl Karsten <carl@personnelware.com>
2017-05-09 09:20:33 -0500

    installer test runs without human.

--- scripts/test_thumb.sh
+++ scripts/test_thumb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 # give user rw to usb device
+# sudo adduser juser disk
 # (need to logout/in for this to take affect.)
 # or do some other trick that I don't think will let the script run :p
-sudo adduser juser disk
 
 # blank disk to install to
 qemu-img create -f qcow2 disk.cow 8G
@@ -14,6 +14,6 @@ qemu-img create -f qcow2 disk.cow 8G
 # watch for the "press esc for boot menu" - hit Esc, 2
 qemu-system-x86_64 -m 256 \
     -drive file=disk.cow,index=0 \
-    -drive file=/dev/sdb,index=1 \
-    -boot menu=on
+    -drive file=/dev/sdb,index=1,format=raw,if=none,id=thumb \
+      -device ide-hd,drive=thumb,bootindex=1
 
