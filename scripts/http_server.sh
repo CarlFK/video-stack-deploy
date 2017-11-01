69a5d74edb4a77639741adaecdd852013ac0cdc6
Carl Karsten <carl@personnelware.com>
2017-11-01 17:56:30 -0500

    disable local squid cache becuase it uses the same 8000 port as the python http server

--- scripts/http_server.sh
+++ scripts/http_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
-# serves preseed and late_command
+# serves d-i/foo/preseed, ec/early_command.sh and d-i/late_command.sh
 
+# turn off local cache that runs on the same 8000 port
+sudo systemctl stop squid-deb-proxy.service
+
+cd ../roles/tftp-server/files/
 python -m SimpleHTTPServer
