6acda1fb2eeab9c2e775fd356da464c42767ef13
Carl Karsten <carl@personnelware.com>
2017-11-26 16:19:21 -0600

    add voctotests role

---
+++ roles/voctotests/files/test1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+# Mostly to confirm the basics is installed and setup properly.
+# test1.sh expects git checkouts, test2 installed on $PATH
+
+voctocore -i test1.ini &
+sleep 5
+voctogui &
+voctomix-ingest &
+sleep 1
+voctomix-ingest --port 10001 &
+voctomix-record-timestamp &
+voctomix-generate-cut-list
