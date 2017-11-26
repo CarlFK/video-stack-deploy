fac89093a46fc74e1df5750830eb6059abb4c2fb
Carl Karsten <carl@personnelware.com>
2017-11-26 16:03:46 -0600

    move SimpleHTTPServer port to 8007 (again)

--- scripts/http_server.sh
+++ scripts/http_server.sh
@@ -2,8 +2,5 @@
 
 # serves d-i/foo/preseed, ec/early_command.sh and d-i/late_command.sh
 
-# turn off local cache that runs on the same 8000 port
-sudo systemctl stop squid-deb-proxy.service
-
 cd ../roles/tftp-server/files/
-python -m SimpleHTTPServer
+python -m SimpleHTTPServer 8007
