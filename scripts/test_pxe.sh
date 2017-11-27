22c19123b61ebb92efdc3f313ab40cebb8c1468d
Carl Karsten <carl@personnelware.com>
2017-11-27 02:48:14 -0600

    script for testing pxe boot with vm

---
+++ scripts/test_pxe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ex
+
+lan_dev=p3p1
+
+# sudo apt install qemu brctl
+
+qemu-img create -f vmdk cache/testpxe.vmdk 10
+
+sudo ip tuntap add dev virttap mode tap user $USER
+sudo brctl addbr virtbr
+
+sudo brctl addif virtbr $lan_dev
+sudo brctl addif virtbr virttap
+sudo ip link set virtbr up
+sudo ip link set $lan_dev up
+sudo ip link set virttap up
+
+qemu-system-x86_64 -m 1024 -hda cache/testpxe.vmdk \
+	  -boot n \
+	  -option-rom /usr/share/qemu/pxe-rtl8139.rom \
+	  -net nic \
+	  -net tap,ifname=virttap,script=no,downscript=no
+
+# -display curses
+
